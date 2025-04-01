10dee6e3ececf1447b0efdea971a0b6a30068a0e
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-04-01 17:43:58 +0200

    Add images to About Us and Posts pages for enhanced visual appeal

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -10,6 +10,10 @@ export default function AboutUs() {
                         <p>
                             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta distinctio quibusdam, reiciendis, at vero mollitia magnam praesentium porro doloremque nulla in non nam quam est corporis sed quae quasi accusantium inventore, quod earum. Repudiandae veniam, porro assumenda quibusdam illo vitae sequi placeat soluta quae voluptatibus quo commodi nisi! Ad, iure optio nemo quae corporis temporibus perspiciatis aspernatur totam, impedit quam incidunt neque quibusdam. Aperiam nisi tempora quidem quod. Itaque, eaque id! Consequuntur, architecto! Dolorem minima placeat debitis at porro? Debitis iusto ipsam quas consectetur consequuntur mollitia fugiat magnam, amet sint odit consequatur, labore id, sequi nobis iure inventore officia laborum?
                         </p>
+                        <div className="d-flex justify-content-center">
+                            <img src="https://fastly.picsum.photos/id/106/2592/1728.jpg?hmac=E1-3Hac5ffuCVwYwexdHImxbMFRsv83exZ2EhlYxkgY" alt="" width={700} />
+                        </div>
+
                     </div>
                 </section>
             </main>
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -8,6 +8,7 @@ export default function Posts() {
                         <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-3">
                             <div className="col d-flex">
                                 <div className="card">
+                                    <img className="img-top" src="https://fastly.picsum.photos/id/37/2000/1333.jpg?hmac=vpYLNsQZwU2szsZc4Uo17cW786vR0GEUVq4icaKopQI" alt="" />
                                     <div className="card-body">
                                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos excepturi odit magni perferendis obcaecati consectetur, vero non labore itaque atque.
                                     </div>
@@ -15,6 +16,7 @@ export default function Posts() {
                             </div>
                             <div className="col">
                                 <div className="card">
+                                    <img className="img-top" src="https://fastly.picsum.photos/id/38/1280/960.jpg?hmac=HBrgyJHQOGVicaWoXgvdSfTakkAyv4BxAt4rF0DhWkU" alt="" />
                                     <div className="card-body">
                                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos excepturi odit magni perferendis obcaecati consectetur, vero non labore itaque atque.
                                     </div>
@@ -22,6 +24,7 @@ export default function Posts() {
                             </div>
                             <div className="col">
                                 <div className="card">
+                                    <img className="img-top" src="https://fastly.picsum.photos/id/54/3264/2176.jpg?hmac=blh020fMeJ5Ru0p-fmXUaOAeYnxpOPHnhJojpzPLN3g" alt="" />
                                     <div className="card-body">
                                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos excepturi odit magni perferendis obcaecati consectetur, vero non labore itaque atque.
                                     </div>
@@ -29,6 +32,7 @@ export default function Posts() {
                             </div>
                             <div className="col">
                                 <div className="card">
+                                    <img className="img-top" src="https://fastly.picsum.photos/id/58/1280/853.jpg?hmac=YO3QnOm9TpyM5DqsJjoM4CHg8oIq4cMWLpd9ALoP908" alt="" />
                                     <div className="card-body">
                                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos excepturi odit magni perferendis obcaecati consectetur, vero non labore itaque atque.
                                     </div>
@@ -36,6 +40,7 @@ export default function Posts() {
                             </div>
                             <div className="col">
                                 <div className="card">
+                                    <img className="img-top" src="https://fastly.picsum.photos/id/62/2000/1333.jpg?hmac=PbFIn8k0AndjiUwpOJcfHz2h-wPCQi_vJRTJZPdr6kQ" alt="" />
                                     <div className="card-body">
                                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos excepturi odit magni perferendis obcaecati consectetur, vero non labore itaque atque.
                                     </div>
@@ -43,6 +48,7 @@ export default function Posts() {
                             </div>
                             <div className="col">
                                 <div className="card">
+                                    <img className="img-top" src="https://fastly.picsum.photos/id/66/3264/2448.jpg?hmac=H9yvGug9-Lk5f-1qZqs6dEV-Yd40jFOIC7oudo4eBK4" alt="" />
                                     <div className="card-body">
                                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos excepturi odit magni perferendis obcaecati consectetur, vero non labore itaque atque.
                                     </div>
