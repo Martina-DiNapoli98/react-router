4d5b5035a2d0c53f355386608895b6a140f5e892
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-04-01 17:58:20 +0200

    Add images and buttons to Home page cards for enhanced interactivity

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,45 +21,57 @@ export default function Home() {
                     <div className="container">
                         <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-3">
                             <div className="col d-flex">
-                                <div className="card">
+                                <div className="card m-4">
+                                    <img className="img-top" src="https://fastly.picsum.photos/id/128/3823/2549.jpg?hmac=VbPyA2vESva2YdoXqll9REBcbQIskgv_c-D60C1s0xc" alt="" />
                                     <div className="card-body">
                                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos excepturi odit magni perferendis obcaecati consectetur, vero non labore itaque atque.
                                     </div>
+                                    <button className="btn btn-primary">Buy Now</button>
                                 </div>
                             </div>
                             <div className="col">
-                                <div className="card">
+                                <div className="card m-4">
+                                    <img className="img-top" src="https://fastly.picsum.photos/id/141/2048/1365.jpg?hmac=xcZmN-VINP95YwmI3AXCTIxfzd61fhaczQmodKQjLbY" alt="" />
                                     <div className="card-body">
                                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos excepturi odit magni perferendis obcaecati consectetur, vero non labore itaque atque.
                                     </div>
+                                    <button className="btn btn-primary">Buy Now</button>
                                 </div>
                             </div>
                             <div className="col">
-                                <div className="card">
+                                <div className="card m-4">
+                                    <img className="img-top" src="https://fastly.picsum.photos/id/170/2500/1667.jpg?hmac=_-EP0GQDcU9M00VvD5FFfDBbFbmSIjgn_Wi4HJJ65HQ" alt="" />
                                     <div className="card-body">
                                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos excepturi odit magni perferendis obcaecati consectetur, vero non labore itaque atque.
                                     </div>
+                                    <button className="btn btn-primary">Buy Now</button>
                                 </div>
                             </div>
                             <div className="col">
-                                <div className="card">
+                                <div className="card m-4">
+                                    <img className="img-top" src="https://fastly.picsum.photos/id/184/4288/2848.jpg?hmac=l0fKWzmWf6ISTPMEm1WjRdxn35sg6U3GwZLn5lvKhTI" alt="" />
                                     <div className="card-body">
                                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos excepturi odit magni perferendis obcaecati consectetur, vero non labore itaque atque.
                                     </div>
+                                    <button className="btn btn-primary">Buy Now</button>
                                 </div>
                             </div>
                             <div className="col">
-                                <div className="card">
+                                <div className="card m-4">
+                                    <img className="img-top" src="https://fastly.picsum.photos/id/222/1800/977.jpg?hmac=pd-vkWA544ktuiSTDnx0Btezs9lMx99cvCU4KVzFBmI" alt="" />
                                     <div className="card-body">
                                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos excepturi odit magni perferendis obcaecati consectetur, vero non labore itaque atque.
                                     </div>
+                                    <button className="btn btn-primary">Buy Now</button>
                                 </div>
                             </div>
                             <div className="col">
-                                <div className="card">
+                                <div className="card m-4">
+                                    <img className="img-top" src="https://fastly.picsum.photos/id/199/2592/1728.jpg?hmac=qOzjPDUZT5vWXyaHaaTo602LZZdZgyQtSfa8KB3uTDs" alt="" />
                                     <div className="card-body">
                                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos excepturi odit magni perferendis obcaecati consectetur, vero non labore itaque atque.
                                     </div>
+                                    <button className="btn btn-primary">Buy Now</button>
                                 </div>
                             </div>
                         </div>
